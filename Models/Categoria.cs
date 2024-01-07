38f07c2117c33c10e0559991d6ffc834c64ca26a
Leo0o00 <castillofernandezleonardo778@gmail.com>
2024-01-06 22:57:52 -0600

    Utilizando el metodo Fluent API de Entitity Framework se añaden de manera mas sencilla a traves de funciones anidadas los atributos a las diferentes propiedades que conformaran los campos en las tablas.

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -4,15 +4,15 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Minimal_API.Models
+namespace Fluent_API.Models
 {
     public class Categoria
     {
-        [Key]
+        /*[Key]*/    //Estos atributos son reemplazados por las funciones de la clase OnModelCreating
         public Guid CategoriaId { get; set;}
         
-        [Required]  //Hace que este campo sea requerido obligatoriamente
-        [MaxLength(150)]  //Establece un maximo de caracteres para este campo
+        /*[Required]*/  //Hace que este campo sea requerido obligatoriamente
+        /*[MaxLength(150)]*/  //Establece un maximo de caracteres para este campo
         public String Nombre { get; set; }
         public String Descripcion { get; set; }
         
